babff8119413b304af6c4086f97147d343baa424
triggerderlerr <avaburak17@gmail.com>
2024-05-15 23:24:48 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,9 +8,6 @@ export const express = require('express')
 export const app = express()
 export const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
