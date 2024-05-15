2dfe3ca873814673dce059cb4eada20115f92661
triggerderlerr <avaburak17@gmail.com>
2024-05-15 22:11:52 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,58 +1,59 @@
-import { GatewayIntentBits, Collection, ButtonStyle } from 'discord.js';
+import { GatewayIntentBits, Collection, ButtonStyle } from "discord.js";
 
 export const internal = {
-    token: process.env.TOKEN, //Bot token (https://discord.com/developers/applications)
-}
+  token: process.env.TOKEN, //Bot token (https://discord.com/developers/applications)
+};
 
+export const intents: GatewayIntentBits[] | number = 131071; // all intent
 
-export const intents: GatewayIntentBits[] | number = 131071 // all intent
-
-export const cooldownVoiceJoin: number = 1000 // Movement delay
+export const cooldownVoiceJoin: number = 1000; // Movement delay
 
 export const settings = {
-    webhook : {
-        name: 'Voice Manager' // Webhook name
+  webhook: {
+    name: "Ses Asistanı", // Webhook name
+  },
+  defaultName: "⭐ {username}",
+  color: 0x2f3136,
+  style: ButtonStyle.Secondary,
+  buttons: {
+    // Emoji and their description :)
+    rename: {
+      title: "Kanal Adı Değiştir",
     },
-    defaultName: '⭐ {username}',
-    color: 0x2f3136,
-    style: ButtonStyle.Secondary,
-    buttons: { // Emoji and their description :)
-        'rename': {
-            title: 'Edit Channel Name'
-        },
-        'limit': {
-            title: 'Change Limit'
-        },
-        'close': {
-            title: 'Close/Open'
-        },
-        'hide': {
-            title: 'Hide/Show'
-        },
-        'user': {
-            title: 'Ban/Unban'
-        },
-        'speak': {
-            title: 'Mute/Unmute'
-        },
-        'kick': {
-            title: 'Kick User'
-        },
-        'reset': {
-         title: 'Reset User Permissions'
-         },
-        'owner': {
-            title: 'Transfer owner'
-        },
-        'info': {
-        title: 'Room Information'
-        }
+    limit: {
+      title: "Limit Değiştir",
     },
-    placeholder: { // Titles for the menu when a user or channel is selected
-        user: '🔷 Select user',
-        channel: '🔷 Choose a private room'
+    close: {
+      title: "Aç/Kapat",
     },
-    line: true, // Line in the control panel (now it is not worth it, to put it, enter "true" instead of "false")
-    dot: false, // Emoji in front of emoji in the room panel (there is no emoji now, so type "true" instead of "false")
-    debug: false // Debugging (now it is not worth it, to put it, enter "true" instead of "false")
-}
+    hide: {
+      title: "Göster/Gizle",
+    },
+    user: {
+      title: "Ban/Unban",
+    },
+    speak: {
+      title: "Mute/Unmute",
+    },
+    kick: {
+      title: "Kullanıcı At",
+    },
+    reset: {
+      title: "Izinleri Sıfırla",
+    },
+    owner: {
+      title: "Odayı Aktar",
+    },
+    info: {
+      title: "Oda Bilgisi",
+    },
+  },
+  placeholder: {
+    // Titles for the menu when a user or channel is selected
+    user: "🔷 Kullanıcı seç",
+    channel: "🔷 Özel oda seç",
+  },
+  line: true, // Line in the control panel (now it is not worth it, to put it, enter "true" instead of "false")
+  dot: false, // Emoji in front of emoji in the room panel (there is no emoji now, so type "true" instead of "false")
+  debug: false, // Debugging (now it is not worth it, to put it, enter "true" instead of "false")
+};
