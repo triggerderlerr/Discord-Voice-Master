ec0739bc2b4841ae34daa11eb1ef6ca973b3a058
triggerderlerr <avaburak17@gmail.com>
2024-05-15 22:46:02 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,23 @@ export const internal = {
   token: process.env.TOKEN, //Bot token (https://discord.com/developers/applications)
 };
 
+import * as k8s from '@kubernetes/client-node';
+import * as net from 'net';
+
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+
+const forward = new k8s.PortForward(kc);
+
+// This simple server just forwards traffic from itself to a service running in kubernetes
+// -> localhost:8080 -> port-forward-tunnel -> kubernetes-pod
+// This is basically equivalent to 'kubectl port-forward ...' but in TypeScript.
+const server = net.createServer((socket) => {
+    forward.portForward('default', 'demo', [8080], socket, null, socket);
+});
+
+server.listen(8080, '127.0.0.1');
+
 export const intents: GatewayIntentBits[] | number = 131071; // all intent
 
 export const cooldownVoiceJoin: number = 1000; // Movement delay
